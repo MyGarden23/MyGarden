83e18d878d17b023f1cdbda10d73497421b33f0d
matt-opw <matossipow@icloud.com>
2025-10-06 19:43:59 +0200

    test(theme): add instrumented tests for status bar color in MyGardenTheme
    Added MyGardenThemeStatusBarInstrumentedTest to verify that the status bar color is correctly applied based on the current theme mode.

--- app/src/test/java/com/android/sample/theme/MyGardenThemeColorTest.kt
+++ app/src/test/java/com/android/sample/theme/MyGardenThemeColorTest.kt
@@ -1,13 +1,72 @@
-package com.android.sample.ui.theme
+package com.android.sample.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.test.junit4.createComposeRule
+import com.android.sample.ui.theme.MyGardenTheme
+import com.android.sample.ui.theme.md_theme_dark_background
+import com.android.sample.ui.theme.md_theme_dark_error
+import com.android.sample.ui.theme.md_theme_dark_errorContainer
+import com.android.sample.ui.theme.md_theme_dark_inverseOnSurface
+import com.android.sample.ui.theme.md_theme_dark_inversePrimary
+import com.android.sample.ui.theme.md_theme_dark_inverseSurface
+import com.android.sample.ui.theme.md_theme_dark_onBackground
+import com.android.sample.ui.theme.md_theme_dark_onError
+import com.android.sample.ui.theme.md_theme_dark_onErrorContainer
+import com.android.sample.ui.theme.md_theme_dark_onPrimary
+import com.android.sample.ui.theme.md_theme_dark_onPrimaryContainer
+import com.android.sample.ui.theme.md_theme_dark_onSecondary
+import com.android.sample.ui.theme.md_theme_dark_onSecondaryContainer
+import com.android.sample.ui.theme.md_theme_dark_onSurface
+import com.android.sample.ui.theme.md_theme_dark_onSurfaceVariant
+import com.android.sample.ui.theme.md_theme_dark_onTertiary
+import com.android.sample.ui.theme.md_theme_dark_onTertiaryContainer
+import com.android.sample.ui.theme.md_theme_dark_outline
+import com.android.sample.ui.theme.md_theme_dark_outlineVariant
+import com.android.sample.ui.theme.md_theme_dark_primary
+import com.android.sample.ui.theme.md_theme_dark_primaryContainer
+import com.android.sample.ui.theme.md_theme_dark_scrim
+import com.android.sample.ui.theme.md_theme_dark_secondary
+import com.android.sample.ui.theme.md_theme_dark_secondaryContainer
+import com.android.sample.ui.theme.md_theme_dark_surface
+import com.android.sample.ui.theme.md_theme_dark_surfaceTint
+import com.android.sample.ui.theme.md_theme_dark_surfaceVariant
+import com.android.sample.ui.theme.md_theme_dark_tertiary
+import com.android.sample.ui.theme.md_theme_dark_tertiaryContainer
+import com.android.sample.ui.theme.md_theme_light_background
+import com.android.sample.ui.theme.md_theme_light_error
+import com.android.sample.ui.theme.md_theme_light_errorContainer
+import com.android.sample.ui.theme.md_theme_light_inverseOnSurface
+import com.android.sample.ui.theme.md_theme_light_inversePrimary
+import com.android.sample.ui.theme.md_theme_light_inverseSurface
+import com.android.sample.ui.theme.md_theme_light_onBackground
+import com.android.sample.ui.theme.md_theme_light_onError
+import com.android.sample.ui.theme.md_theme_light_onErrorContainer
+import com.android.sample.ui.theme.md_theme_light_onPrimary
+import com.android.sample.ui.theme.md_theme_light_onPrimaryContainer
+import com.android.sample.ui.theme.md_theme_light_onSecondary
+import com.android.sample.ui.theme.md_theme_light_onSecondaryContainer
+import com.android.sample.ui.theme.md_theme_light_onSurface
+import com.android.sample.ui.theme.md_theme_light_onSurfaceVariant
+import com.android.sample.ui.theme.md_theme_light_onTertiary
+import com.android.sample.ui.theme.md_theme_light_onTertiaryContainer
+import com.android.sample.ui.theme.md_theme_light_outline
+import com.android.sample.ui.theme.md_theme_light_outlineVariant
+import com.android.sample.ui.theme.md_theme_light_primary
+import com.android.sample.ui.theme.md_theme_light_primaryContainer
+import com.android.sample.ui.theme.md_theme_light_scrim
+import com.android.sample.ui.theme.md_theme_light_secondary
+import com.android.sample.ui.theme.md_theme_light_secondaryContainer
+import com.android.sample.ui.theme.md_theme_light_surface
+import com.android.sample.ui.theme.md_theme_light_surfaceTint
+import com.android.sample.ui.theme.md_theme_light_surfaceVariant
+import com.android.sample.ui.theme.md_theme_light_tertiary
+import com.android.sample.ui.theme.md_theme_light_tertiaryContainer
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
-class MyGardenThemeInstrumentedTest {
+class MyGardenThemeTest {
 
   @get:Rule val composeRule = createComposeRule()
 
