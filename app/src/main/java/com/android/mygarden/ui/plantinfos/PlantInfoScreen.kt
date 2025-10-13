a97624bab774ed53da022a1d35b49ee33dd88bf2
kulkix <149168489+kulkix@users.noreply.github.com>
2025-10-13 16:29:40 +0200

    chore: change the image of a Plant from Image? to String
    * chore: change the image of a Plant from Image? to String
    
    We now store the path to the image of the plant and not directely the image

--- app/src/main/java/com/android/mygarden/ui/plantinfos/PlantInfoScreen.kt
+++ app/src/main/java/com/android/mygarden/ui/plantinfos/PlantInfoScreen.kt
@@ -256,7 +256,7 @@ fun PlantDetailScreenPreview() {
   val plant =
       Plant(
           name = "test name",
-          image = null, // Can use a URL or a ByteArray for storage (to be discussed)
+          image = null,
           latinName = "latin name",
           description = "Description of the plant",
           healthStatus = PlantHealthStatus.HEALTHY,
