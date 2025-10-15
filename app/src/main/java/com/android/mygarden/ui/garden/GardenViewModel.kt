ed66ce059738102c6a63a68aec0811cb1e014852
pippolin0 <160582937+pippolin0@users.noreply.github.com>
2025-10-15 18:31:53 +0200

    Feature/garden screen (#42)
    * feat(garden): implement garden screen UI and ViewModel logic
    
    - Added GardenScreen composable displaying user info and plant list
    
    - Implemented GardenViewModel to manage UI state
    
    - Added support for displaying owned plants with dynamic status color
    
    * chore(icons): add drawable resources for garden screen UI
    
    - Added new vector icons used in the GardenScreen layout
    
    - Includes tree, edit, potted plant, and profile placeholder icons
    
    * test(garden): add unit and UI tests for garden screen and view model
    
    - Added GardenScreenTests to verify UI behavior and component visibility
    
    - Added GardenViewModelTests to validate state management and repository calls
    
    - Used local repository for isolated test environment
    
    - Ensured consistent setup and cleanup across all garden tests
    
    * docs(tests): finish the documentation for the test files
    
    * chore(formatting): reformat test files
    
    * fix(model): fix the local repository
    
    fix the getAllOwnedPlants function that now returns a copy of the list, not the list itself, for consistency.
    
    * fix: addressed PR feedback
    
    * chore(formatting): reformat files

---
+++ app/src/main/java/com/android/mygarden/ui/garden/GardenViewModel.kt
@@ -0,0 +1,69 @@
+package com.android.mygarden.ui.garden
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.mygarden.model.plant.OwnedPlant
+import com.android.mygarden.model.plant.PlantsRepository
+import com.android.mygarden.model.plant.PlantsRepositoryProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+/**
+ * Represents the UI state of the garden
+ *
+ * @property plants the list of plants owned by the current user : empty by default
+ * @property errorMsg a potential error message to be displayed if one of the actions of the view
+ *   model with the repository doesn't work
+ */
+data class GardenUIState(val plants: List<OwnedPlant> = emptyList(), val errorMsg: String? = null)
+
+/**
+ * The view model of the garden that handles all UI interactions
+ *
+ * @property plantsRepo the repository of the plants to store them
+ */
+class GardenViewModel(
+    private val plantsRepo: PlantsRepository = PlantsRepositoryProvider.repository,
+) : ViewModel() {
+  private val _uiState = MutableStateFlow(GardenUIState())
+  val uiState: StateFlow<GardenUIState> = _uiState.asStateFlow()
+
+  init {
+    refreshUIState()
+  }
+
+  /** Refresh the UI state by fetching all the owned plants */
+  fun refreshUIState() {
+    getAllPlants()
+  }
+
+  /** Clears the error message of the UI state */
+  fun clearErrorMsg() {
+    _uiState.value = _uiState.value.copy(errorMsg = null)
+  }
+
+  /**
+   * Sets the error message of the UI state
+   *
+   * @param msg the error message
+   */
+  fun setErrorMsg(msg: String) {
+    _uiState.value = _uiState.value.copy(errorMsg = msg)
+  }
+
+  /** Fetches all the plants from the [plantsRepo] and set an error message if the fetch failed */
+  fun getAllPlants() {
+    viewModelScope.launch {
+      try {
+        val plants = plantsRepo.getAllOwnedPlants()
+        _uiState.value = GardenUIState(plants = plants)
+      } catch (e: Exception) {
+        Log.e("GardenViewModel", "Owned plants couldn't be retrieved from repository", e)
+        setErrorMsg("getAllPlants failed : Owned plants couldn't be retrieved from repository")
+      }
+    }
+  }
+}
---
+++ app/src/test/java/com/android/mygarden/ui/garden/GardenViewModelTests.kt
@@ -0,0 +1,86 @@
+package com.android.mygarden.ui.garden
+
+import com.android.mygarden.model.plant.OwnedPlant
+import com.android.mygarden.model.plant.Plant
+import com.android.mygarden.model.plant.PlantHealthStatus
+import com.android.mygarden.model.plant.PlantsRepository
+import com.android.mygarden.model.plant.PlantsRepositoryLocal
+import java.sql.Timestamp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GardenViewModelTests {
+
+  private lateinit var repo: PlantsRepository
+  private lateinit var vm: GardenViewModel
+  private lateinit var ownedPlant: OwnedPlant
+  val plant1 =
+      Plant(
+          "hello",
+          null,
+          "laurem ipsum",
+          "beautiful plant",
+          PlantHealthStatus.HEALTHY,
+          "is healthy",
+          2)
+  private val testDispatcher = StandardTestDispatcher()
+
+  /** Sets up the repository and the view model and the test dispatcher to simulate the app */
+  @Before
+  fun setUp() {
+    Dispatchers.setMain(testDispatcher)
+    repo = PlantsRepositoryLocal()
+    vm = GardenViewModel(plantsRepo = repo)
+  }
+
+  /** Ensures the reset of the dispatcher at each end of test */
+  @After
+  fun end() {
+    Dispatchers.resetMain()
+  }
+
+  /** Tests that the setErrorMessage function updates correctly the vm */
+  @Test
+  fun setErrorMsgUpdatesCorrectly() {
+    val errorMsg = "Error"
+    vm.setErrorMsg(errorMsg)
+    assertEquals(errorMsg, vm.uiState.value.errorMsg)
+  }
+
+  /** Tests that the clearErrorMessage function updates correctly the vm */
+  @Test
+  fun clearErrorMsgClearsCorrectly() {
+    val errorMsg = "Error"
+    vm.setErrorMsg(errorMsg)
+    assertEquals(errorMsg, vm.uiState.value.errorMsg)
+    vm.clearErrorMsg()
+    assertNull(vm.uiState.value.errorMsg)
+  }
+
+  /**
+   * Tests that the getAllPlants function retrieves correctly the list of owned plants and updated
+   * the vm
+   */
+  @Test
+  fun getAllPlantsWorksCorrectly() = runTest {
+    assertEquals(emptyList<OwnedPlant>(), vm.uiState.value.plants)
+    vm.getAllPlants()
+    advanceUntilIdle()
+    assertEquals(emptyList<OwnedPlant>(), vm.uiState.value.plants)
+    ownedPlant = repo.saveToGarden(plant1, repo.getNewId(), Timestamp(1))
+    advanceUntilIdle()
+    vm.getAllPlants()
+    advanceUntilIdle()
+    assertEquals(listOf(ownedPlant), vm.uiState.value.plants)
+  }
+}
