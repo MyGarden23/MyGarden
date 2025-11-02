eb4b8d9448ffbb4b1c700ec392c19c62974a92bc
mattmeyer1066 <matteo.meyer@epfl.ch>
2025-11-02 21:32:19 +0100

    feat: Update main activity navigation to include edit profile route

--- app/src/main/java/com/android/mygarden/MainActivity.kt
+++ app/src/main/java/com/android/mygarden/MainActivity.kt
@@ -136,7 +136,7 @@ private fun routeToScreen(route: String): Screen? =
       Screen.Camera.route -> Screen.Camera
       Screen.PlantView.route -> Screen.PlantView
       Screen.NewProfile.route -> Screen.NewProfile
-      Screen.EditPlant.route -> Screen.EditPlant
+      Screen.EditProfile.route -> Screen.EditProfile
       Screen.Profile.route -> Screen.Profile
       Screen.Garden.route -> Screen.Garden
       Screen.ChooseAvatar.route -> Screen.ChooseAvatar
