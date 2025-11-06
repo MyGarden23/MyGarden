24c2c4f79a55292115befd4231cb9400cf1c182f
Ulysse <kulkix2000@gmail.com>
2025-11-06 20:32:07 +0100

    feature: PlantsRepositoryFirestore.kt saves the image in Cloud Storage for Firestore

--- app/src/main/java/com/android/mygarden/model/plant/PlantsRepositoryFirestore.kt
+++ app/src/main/java/com/android/mygarden/model/plant/PlantsRepositoryFirestore.kt
@@ -1,6 +1,7 @@
 package com.android.mygarden.model.plant
 
 import android.net.Uri
+import android.util.Log
 import com.android.mygarden.model.plant.FirestoreMapper.fromOwnedPlantToSerializedOwnedPlant
 import com.android.mygarden.model.plant.FirestoreMapper.fromSerializedOwnedPlantToOwnedPlant
 import com.google.firebase.auth.FirebaseAuth
@@ -74,7 +75,19 @@ class PlantsRepositoryFirestore(
   private fun userPlantsCollection() =
       firestore.collection(usersCollection).document(currentUserId()).collection(plantsCollection)
 
+  /** The access to Cloud Storage for Firestore of type FirebaseStorage. */
   private val storage: FirebaseStorage = FirebaseStorage.getInstance()
+  /**
+   * Gives the reference to the image associated to the id given in argument in the current user's
+   * plants collection in Cloud Storage.
+   *
+   * @param plantId the name of the plant associated to the image
+   * @return the storage reference in Cloud Storage of the image
+   */
+  private fun storageRef(plantId: String): StorageReference {
+    return storage.reference.child(
+        "$usersCollection/${currentUserId()}/$plantsCollection/$plantId.jpg")
+  }
 
   /** The id of the current user. Throw IllegalStateException if the user is not authenticated. */
   private fun currentUserId(): String {
@@ -87,8 +100,12 @@ class PlantsRepositoryFirestore(
   }
 
   override suspend fun saveToGarden(plant: Plant, id: String, lastWatered: Timestamp): OwnedPlant {
-    val ownedPlant = OwnedPlant(id, plant, lastWatered)
+    // Upload the image to Cloud Storage
+    val imageUrl: String? = uploadLocalImageToCloudStorage(plant.image, id)
 
+    // Creates an OwnedPlant with the arguments and change the image field of the plant to store the
+    // URL in Cloud Storage or null if there is no image.
+    val ownedPlant = OwnedPlant(id, plant.copy(image = imageUrl), lastWatered)
     val serializedOwnedPlant = fromOwnedPlantToSerializedOwnedPlant(ownedPlant)
 
     userPlantsCollection().document(id).set(serializedOwnedPlant).await()
@@ -126,9 +143,12 @@ class PlantsRepositoryFirestore(
   }
 
   override suspend fun deleteFromGarden(id: String) {
+    // Delete the data in Firestore
     val document = userPlantsCollection().document(id).get().await()
     if (!document.exists()) throw IllegalArgumentException(plantNotFoundErrMsg(id))
     userPlantsCollection().document(id).delete().await()
+    // Delete the image in Cloud Storage
+    deleteImageInCloudStorge(id)
   }
 
   override suspend fun editOwnedPlant(id: String, newOwnedPlant: OwnedPlant) {
@@ -159,18 +179,52 @@ class PlantsRepositoryFirestore(
     docRef.set(fromOwnedPlantToSerializedOwnedPlant(newOwnedPlant), SetOptions.merge()).await()
   }
 
-  private suspend fun uploadLocalImageToCloudStorage(file: File, plantId: String): String {
+  /**
+   * Uploads the image of the plant (stored locally) to Cloud Storage for Firestore and return the
+   * URL of where this image is stored or null if there was no image to store.
+   *
+   * @param imagePath the path of the image stored locally or null if there is no image to store
+   * @param plantId the id of the plant associated to the image to know where to store the image in
+   *   Cloud Storage
+   * @return the URL of where the image is stored in Cloud Storage or null if there was no image to
+   *   store.
+   */
+  private suspend fun uploadLocalImageToCloudStorage(imagePath: String?, plantId: String): String? {
+    if (imagePath == null) return null
+    val imageFile = File(imagePath)
     // Create a reference in Cloud Storage
-    val storageRef: StorageReference =
-        storage.reference.child(
-            "$usersCollection/${currentUserId()}/$plantsCollection/$plantId.jpg")
-    val fileUri = Uri.fromFile(file)
+    val storageReference = storageRef(plantId)
+    val fileUri = Uri.fromFile(imageFile)
 
     // Upload the File
-    storageRef.putFile(fileUri).await()
+    storageReference.putFile(fileUri).await()
+
+    // Delete the the image locally
+    if (imageFile.exists()) {
+      val deleted = imageFile.delete()
+      if (!deleted) {
+        Log.w(
+            "PlantsRepositoryFirestore",
+            "Failed to delete the local file image: ${imageFile.path} ")
+      }
+    }
 
     // Get the URL
-    return storageRef.downloadUrl.await().toString()
+    return storageReference.downloadUrl.await().toString()
+  }
+
+  /**
+   * Delete an image referred by the id of the plant it is associated to from Cloud Storage
+   *
+   * @param plantId the name of the image in Cloud Storage
+   */
+  private suspend fun deleteImageInCloudStorge(plantId: String) {
+    val storageReference = storageRef(plantId)
+    try {
+      storageReference.delete().await()
+    } catch (e: Exception) {
+      Log.w("Cloud Storage", "Image not found or already deleted: ${e.message}")
+    }
   }
 
   /**
