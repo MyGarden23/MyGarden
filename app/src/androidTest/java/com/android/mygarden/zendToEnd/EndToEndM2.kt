a7e052a36a3a64651ee32854f7b8dc1d45437605
mattmeyer1066 <matteo.meyer@epfl.ch>
2025-11-12 21:48:07 +0100

    feat: Improve EndToEndM2

--- app/src/androidTest/java/com/android/mygarden/zendToEnd/EndToEndM2.kt
+++ app/src/androidTest/java/com/android/mygarden/zendToEnd/EndToEndM2.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextClearance
 import androidx.compose.ui.test.performTextInput
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
@@ -37,7 +38,8 @@ class EndToEndM2 : FirestoreProfileTest() {
     }
   }
 
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule
+  val composeTestRule = createAndroidComposeRule<MainActivity>()
 
   /**
    * Automatically grants camera permission before tests run. Essential for camera functionality
@@ -46,14 +48,18 @@ class EndToEndM2 : FirestoreProfileTest() {
   @get:Rule
   val permissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA)
 
+  //TODO add rule for notification permission
+
   private val TIMEOUT = 10_000L
+
+  private val mockPlant = Plant(name = "Rose", latinName = "Rosa")
 
   @Before
   override fun setUp() {
     super.setUp()
     // Set up any necessary configurations or states before each test
     val fakePlantRepoUtils = FakePlantRepositoryUtils(PlantRepositoryType.PlantRepoFirestore)
-    fakePlantRepoUtils.mockIdentifyPlant(Plant(name = "Rose", latinName = "Rosa"))
+    fakePlantRepoUtils.mockIdentifyPlant(mockPlant)
     fakePlantRepoUtils.setUpMockRepo()
   }
 
@@ -71,45 +77,52 @@ class EndToEndM2 : FirestoreProfileTest() {
     composeTestRule.onNodeWithTag(ProfileScreenTestTags.FIRST_NAME_FIELD).performTextInput("John")
     composeTestRule.onNodeWithTag(ProfileScreenTestTags.LAST_NAME_FIELD).performTextInput("Doe")
     composeTestRule
-        .onNodeWithTag(ProfileScreenTestTags.COUNTRY_FIELD)
-        .performTextInput("Switzerland")
+      .onNodeWithTag(ProfileScreenTestTags.COUNTRY_FIELD)
+      .performTextInput("Switzerland")
     composeTestRule.onNodeWithTag(ProfileScreenTestTags.SAVE_BUTTON).performClick()
-
-    // === CAMERA SCREEN ===
-    composeTestRule.onNodeWithTag(NavigationTestTags.CAMERA_SCREEN).assertIsDisplayed()
-
-    // Wait for camera ready
     composeTestRule.waitUntil(TIMEOUT) {
-      composeTestRule.onNodeWithTag(CameraScreenTestTags.TAKE_PICTURE_BUTTON).isDisplayed()
+      composeTestRule.onNodeWithTag(NavigationTestTags.CAMERA_BUTTON).isDisplayed()
     }
-
-    // Verify UI elements
-    composeTestRule.onNodeWithTag(NavigationTestTags.BOTTOM_BAR).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_BUTTON).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(CameraScreenTestTags.FLIP_CAMERA_BUTTON).assertIsDisplayed()
-
-    // goto MyGarden
-    composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_BUTTON).performClick()
-    composeTestRule.waitUntil(TIMEOUT) {
-      composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_SCREEN).isDisplayed()
-    }
-    composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_SCREEN).assertIsDisplayed()
-    composeTestRule
+      // goto MyGarden
+      composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_BUTTON).performClick()
+      composeTestRule.waitUntil(TIMEOUT) {
+        composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_SCREEN).isDisplayed()
+      }
+      composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_SCREEN).assertIsDisplayed()
+      composeTestRule
         .onNodeWithTag(GardenScreenTestTags.USERNAME)
         .assertIsDisplayed()
         .assertTextContains("John")
 
-    // goto Camera
-    composeTestRule.onNodeWithTag(NavigationTestTags.CAMERA_BUTTON).performClick()
-    composeTestRule.onNodeWithTag(CameraScreenTestTags.TAKE_PICTURE_BUTTON).assertIsDisplayed()
-    // Take photo
-    composeTestRule.onNodeWithTag(CameraScreenTestTags.TAKE_PICTURE_BUTTON).performClick()
-
-    // === PLANT INFO SCREEN ===
+      // goto edit profile screen
+      composeTestRule.onNodeWithTag(GardenScreenTestTags.EDIT_PROFILE_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(ProfileScreenTestTags.FIRST_NAME_FIELD).performTextClearance()
+    composeTestRule.onNodeWithTag(ProfileScreenTestTags.FIRST_NAME_FIELD).performTextInput("Ada")
+    composeTestRule.onNodeWithTag(ProfileScreenTestTags.SAVE_BUTTON).performClick()
     composeTestRule.waitUntil(TIMEOUT) {
-      composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.SCREEN).isDisplayed()
+      composeTestRule.onNodeWithTag(NavigationTestTags.GARDEN_SCREEN).isDisplayed()
     }
-    composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.PLANT_NAME).isDisplayed()
-    composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.PLANT_NAME).assertTextContains("Rose")
+    composeTestRule.onNodeWithTag(GardenScreenTestTags.USERNAME).assertIsDisplayed().assertTextContains("Ada")
+
+
+    // goto camera screen
+    composeTestRule.onNodeWithTag(NavigationTestTags.CAMERA_BUTTON).performClick()
+
+      // === CAMERA SCREEN ===
+
+      // Wait for camera ready
+      composeTestRule.waitUntil(TIMEOUT) {
+        composeTestRule.onNodeWithTag(CameraScreenTestTags.TAKE_PICTURE_BUTTON).isDisplayed()
+      }
+
+      // Take photo
+      composeTestRule.onNodeWithTag(CameraScreenTestTags.TAKE_PICTURE_BUTTON).performClick()
+
+      // === PLANT INFO SCREEN ===
+      composeTestRule.waitUntil(TIMEOUT) {
+        composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.SCREEN).isDisplayed()
+      }
+      composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.PLANT_NAME).isDisplayed()
+      composeTestRule.onNodeWithTag(PlantInfoScreenTestTags.PLANT_NAME).assertTextContains("Rose")
+    }
   }
-}
