1e701193074fda274547b80e0f9fca8949248d2e
mattmeyer1066 <matteo.meyer@epfl.ch>
2025-11-13 13:28:07 +0100

    feat: Add utility class for Firebase testing in androidTest suite

---
+++ app/src/androidTest/java/com/android/mygarden/utils/FirebaseUtils.kt
@@ -0,0 +1,58 @@
+package com.android.mygarden.utils
+
+import com.android.mygarden.model.profile.ProfileRepository
+import com.android.mygarden.model.profile.ProfileRepositoryFirestore
+import com.android.mygarden.model.profile.ProfileRepositoryProvider
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.test.runTest
+import okhttp3.internal.wait
+import org.junit.After
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+
+class FirebaseUtils {
+    private lateinit var db: FirebaseFirestore
+    protected lateinit var auth: FirebaseAuth
+    protected lateinit var cred: AuthCredential
+
+    suspend fun initialize() {
+        // Emulators
+        FirebaseEmulator.connectAuth()
+        FirebaseEmulator.clearAuthEmulator()
+        db = FirebaseEmulator.connectFirestore()
+        FirebaseEmulator.clearFirestoreEmulator()
+        auth = FirebaseEmulator.auth
+
+        // Fake sign-in (suspend)
+        auth.signOut()
+        val uniqueEmail = "test.profile+${System.currentTimeMillis()}@example.com"
+        val idToken = FakeJwtGenerator.createFakeGoogleIdToken(email = uniqueEmail)
+        cred = GoogleAuthProvider.getCredential(idToken, null)
+        val result = auth.signInWithCredential(cred).await()
+        val uid = result.user?.uid
+        assertNotNull(uid)
+
+        // Clean user doc (suspend)
+        db.collection("users").document(uid!!).delete().await()
+
+        signOut()
+    }
+
+    fun injectProfileRepository() {
+        ProfileRepositoryProvider.repository = ProfileRepositoryFirestore(db, auth)
+    }
+
+    suspend fun signIn() {
+        val result = auth.signInWithCredential(cred).await()
+        val uid = result.user?.uid
+        assertNotNull(uid)
+    }
+
+    fun signOut() {
+        auth.signOut()
+    }
+}
