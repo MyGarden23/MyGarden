cb7318d8f47ac08ede1127339bb50fd7c140add6
mattmeyer1066 <matteo.meyer@epfl.ch>
2025-11-16 13:47:35 +0100

    feat: Add activity type enum for garden activities

---
+++ app/src/main/java/com/android/mygarden/model/gardenactivity/ActivityType.kt
@@ -0,0 +1,7 @@
+package com.android.mygarden.model.gardenactivity
+
+/** Represents the different types of activities that can be performed in the garden. */
+enum class ActivityType {
+  ADDED_PLANT,
+  ACHIEVEMENT
+}
