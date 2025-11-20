a1c3abf788fdcbdd5ae05d82a0a4b1b26556a689
matt-opw <matossipow@icloud.com>
2025-11-20 11:48:11 +0100

    test(repo): duplicate FakeProfileRepository and FakePseudoRepository for unit test use
    Duplicated FakeProfileRepository and FakePseudoRepository so they can be used both in androidTest and in unit tests.

---
+++ app/src/test/java/com/android/mygarden/utils/FakePseudoRepository.kt
@@ -0,0 +1,12 @@
+package com.android.mygarden.utils
+
+import com.android.mygarden.model.profile.PseudoRepository
+
+/** Fake implementation of PseudoRepository for testing. */
+class FakePseudoRepository() : PseudoRepository {
+  override suspend fun isPseudoAvailable(pseudo: String) = true
+
+  override suspend fun savePseudo(pseudo: String, userId: String) {}
+
+  override suspend fun deletePseudo(pseudo: String) {}
+}
