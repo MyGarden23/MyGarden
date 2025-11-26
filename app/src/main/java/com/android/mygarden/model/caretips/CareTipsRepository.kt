fa738a8285dae43e44857d3ebbad1abcc9124167
fabio.conti <fabio.conti@epfl.ch>
2025-11-26 18:55:15 +0100

    feat(repo): Create tips repo and update plantinfos VM
    - Create the interface CareTipsRepository to ensure correct future modularity (if any)
    
    - Create CareTipsRepositoryProvider for the same reasons
    
    - Create CareTipsRepositoryFirestore to implement the 'cache' used to reduce Gemini API calls
    
    - Update showCareTips in PlantInfoViewModel to check the cache before making the Gemini API call

---
+++ app/src/main/java/com/android/mygarden/model/caretips/CareTipsRepository.kt
@@ -0,0 +1,23 @@
+package com.android.mygarden.model.caretips
+
+import com.android.mygarden.model.plant.PlantHealthStatus
+
+/** Repository interface to manage the cached care tips from Gemini */
+interface CareTipsRepository {
+
+  /**
+   * Gets a tip from the repository ; returns null if it wasn't cached already
+   *
+   * @param latinName the latin name of the plant for which the care tip was called
+   * @param healthStatus the status of the plant for which the care tip was called
+   */
+  suspend fun getTip(latinName: String, healthStatus: PlantHealthStatus): String?
+
+  /**
+   * Adds a tip to the repository, i.e. cache the tip for future requests
+   *
+   * @param latinName the latin name of the plant for which the care tip was called
+   * @param healthStatus the status of the plant for which the care tip was called
+   */
+  suspend fun addTip(latinName: String, healthStatus: PlantHealthStatus, tip: String)
+}
