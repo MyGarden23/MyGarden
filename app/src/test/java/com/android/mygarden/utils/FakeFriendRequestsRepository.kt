6a535e38e8762d5532d8dedf62d49f21ee5251ea
Loann12 <loann.colli@epfl.ch>
2025-12-01 13:08:24 +0100

    test(model): add fake friend requests repository

---
+++ app/src/test/java/com/android/mygarden/utils/FakeFriendRequestsRepository.kt
@@ -0,0 +1,27 @@
+package com.android.mygarden.utils
+
+import com.android.mygarden.model.friends.FriendRequest
+import com.android.mygarden.model.friends.FriendRequestsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+
+/** Minimal fake implementation of [FriendRequestsRepository] for tests. */
+class FakeFriendRequestsRepository : FriendRequestsRepository {
+  var currentUserId: String? = "test-user-id"
+
+  override fun getCurrentUserId(): String? = currentUserId
+
+  override fun myRequests(): Flow<List<FriendRequest>> = MutableStateFlow(emptyList())
+
+  override fun incomingRequests(): Flow<List<FriendRequest>> = MutableStateFlow(emptyList())
+
+  override fun outgoingRequests(): Flow<List<FriendRequest>> = MutableStateFlow(emptyList())
+
+  override suspend fun askFriend(targetUserId: String) {}
+
+  override suspend fun acceptRequest(requestId: String) {}
+
+  override suspend fun refuseRequest(requestId: String) {}
+
+  override fun cleanup() {}
+}
