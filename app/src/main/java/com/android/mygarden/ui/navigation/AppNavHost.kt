d52cc663b7658658342af6f0cbf06f983c1b8b31
Ulysse <kulkix2000@gmail.com>
2025-12-02 19:19:54 +0100

    feature: add the navigation between the feed screen and the friends requests screen
    # Conflicts:
    #	app/src/main/res/values/ui_strings.xml

--- app/src/main/java/com/android/mygarden/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/android/mygarden/ui/navigation/AppNavHost.kt
@@ -22,6 +22,7 @@ import com.android.mygarden.ui.camera.CameraScreen
 import com.android.mygarden.ui.editPlant.EditPlantScreen
 import com.android.mygarden.ui.editPlant.EditPlantViewModel
 import com.android.mygarden.ui.feed.FeedScreen
+import com.android.mygarden.ui.friendsRequests.FriendsRequestsScreen
 import com.android.mygarden.ui.friendList.FriendListScreen
 import com.android.mygarden.ui.garden.GardenScreen
 import com.android.mygarden.ui.plantinfos.PlantInfoViewModel
@@ -166,7 +167,13 @@ fun AppNavHost(
     composable(Screen.Feed.route) {
       FeedScreen(
           onAddFriend = { navigationActions.navTo(Screen.AddFriend) },
-          onFriendList = { navigationActions.navTo(Screen.FriendList) })
+          onFriendList = { navigationActions.navTo(Screen.FriendList) },
+          onNotifClick = { navigationActions.navTo(Screen.FriendsRequests) })
+    }
+
+    // Friends Requests
+    composable(Screen.FriendsRequests.route) {
+      FriendsRequestsScreen(onGoBack = { navigationActions.navBack() })
     }
 
     // Add Friends
