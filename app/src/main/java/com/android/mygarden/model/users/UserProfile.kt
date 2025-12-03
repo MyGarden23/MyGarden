fe65932ebe24a0b53e22c30b2f86e308db4b5116
Simonperraudin <saimp02@gmail.com>
2025-12-03 14:57:03 +0100

    feature(friend list): add attributes gardening skill and favorite plant used for Friends UI card

--- app/src/main/java/com/android/mygarden/model/users/UserProfile.kt
+++ app/src/main/java/com/android/mygarden/model/users/UserProfile.kt
@@ -8,9 +8,13 @@ import com.android.mygarden.ui.profile.Avatar
  * @property id The unique Firestore user ID (document ID under `users/{id}`).
  * @property pseudo The display pseudo associated with the user.
  * @property avatar The avatar enum selected by the user.
+ * @property gardeningSkill the gardening skill of the user
+ * @property favoritePlant the favorite plant of the user
  */
 data class UserProfile(
     val id: String,
     val pseudo: String,
     val avatar: Avatar,
+    val gardeningSkill: String,
+    val favoritePlant: String
 )
