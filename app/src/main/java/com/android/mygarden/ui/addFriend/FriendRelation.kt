33fe45ab71e6664447580da1583dfac70818c2c4
Simonperraudin <saimp02@gmail.com>
2025-12-10 00:28:48 +0100

    feature(friend Relation): move FriendRelation in it's own class for better architecture

---
+++ app/src/main/java/com/android/mygarden/ui/addFriend/FriendRelation.kt
@@ -0,0 +1,25 @@
+package com.android.mygarden.ui.addFriend
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import com.android.mygarden.R
+
+enum class FriendRelation(@StringRes val labelRes: Int) {
+  ADD(R.string.add_enum),
+  ADDED(R.string.added_enum),
+  PENDING(R.string.pending_enum)
+}
+
+@Composable fun FriendRelation.label(): String = stringResource(labelRes)
+
+val FriendRelation.color: Color
+  @Composable
+  get() =
+      when (this) {
+        FriendRelation.ADD -> colorScheme.primary
+        FriendRelation.ADDED -> colorScheme.outline
+        FriendRelation.PENDING -> colorScheme.tertiary
+      }
