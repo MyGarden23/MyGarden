b0aa702e931fd04f442c661314214dc587ed53f4
matt-opw <matossipow@icloud.com>
2025-12-10 08:23:04 +0100

    test: Try to enable Firebase functions in the CI

--- app/src/androidTest/java/com/android/mygarden/model/notifications/FriendRequestsFirebaseNotifierTest.kt
+++ app/src/androidTest/java/com/android/mygarden/model/notifications/FriendRequestsFirebaseNotifierTest.kt
@@ -1,19 +1,22 @@
 package com.android.mygarden.model.notifications
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.mygarden.utils.FirebaseEmulator
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class FriendRequestsFirebaseNotifierTest {
-
+  /*
   @Test
   fun knownTest() {
     assertTrue(true)
-  }
+  }*/
 
-  /*private lateinit var notifier: FirebaseFriendRequestNotifier
+  private lateinit var notifier: FirebaseFriendRequestNotifier
 
   @Before
   fun setUp() {
@@ -29,5 +32,5 @@ class FriendRequestsFirebaseNotifierTest {
     val result = runCatching { notifier.notifyRequestSent(targetUid, fromPseudo) }
 
     assertTrue("Function call should not crash", result.isSuccess)
-  }*/
+  }
 }
