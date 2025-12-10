ce1aad0a311b83f8c177669821701d69d6a6fd99
matt-opw <matossipow@icloud.com>
2025-12-10 08:21:12 +0100

    chore: Add Kdoc in the notifiers and fix sonar Cloud issues

--- app/src/main/java/com/android/mygarden/model/notifications/FirebaseFriendRequestNotifier.kt
+++ app/src/main/java/com/android/mygarden/model/notifications/FirebaseFriendRequestNotifier.kt
@@ -3,6 +3,7 @@ package com.android.mygarden.model.notifications
 import com.google.firebase.functions.FirebaseFunctions
 import kotlinx.coroutines.tasks.await
 
+/** Notifier implemented by Firebase functions. */
 class FirebaseFriendRequestNotifier : FriendRequestNotifier {
   override suspend fun notifyRequestSent(targetUid: String, fromPseudo: String) {
     FirebaseFunctions.getInstance()
