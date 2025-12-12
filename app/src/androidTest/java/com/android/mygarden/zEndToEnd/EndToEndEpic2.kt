67a2d0b342fb5bda5e2d8940ab9dc05420ee68e2
Ulysse <kulkix2000@gmail.com>
2025-12-12 08:30:38 +0100

    fix: inject fake repos for achivement and user profile in the test.

--- app/src/androidTest/java/com/android/mygarden/zEndToEnd/EndToEndEpic2.kt
+++ app/src/androidTest/java/com/android/mygarden/zEndToEnd/EndToEndEpic2.kt
@@ -17,9 +17,11 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
 import com.android.mygarden.MainActivity
+import com.android.mygarden.model.achievements.AchievementsRepositoryProvider
 import com.android.mygarden.model.plant.PlantLocation
 import com.android.mygarden.model.plant.PlantsRepositoryProvider
 import com.android.mygarden.model.plant.testTag
+import com.android.mygarden.model.users.UserProfileRepositoryProvider
 import com.android.mygarden.ui.authentication.SignInScreenTestTags
 import com.android.mygarden.ui.camera.CameraScreenTestTags
 import com.android.mygarden.ui.editPlant.DeletePlantPopupTestTags
@@ -29,7 +31,9 @@ import com.android.mygarden.ui.garden.GardenScreenTestTags
 import com.android.mygarden.ui.navigation.NavigationTestTags
 import com.android.mygarden.ui.plantinfos.PlantInfoScreenTestTags
 import com.android.mygarden.ui.profile.ProfileScreenTestTags
+import com.android.mygarden.utils.FakeAchievementsRepository
 import com.android.mygarden.utils.FakePlantRepositoryUtils
+import com.android.mygarden.utils.FakeUserProfileRepository
 import com.android.mygarden.utils.FirebaseUtils
 import com.android.mygarden.utils.PlantRepositoryType
 import com.android.mygarden.utils.RequiresCamera
@@ -90,6 +94,8 @@ class EndToEndEpic2 {
     fakePlantRepoUtils.mockIdentifyPlant(mockPlant)
     Log.d("EndToEndEpic2", "Set up mock repo")
     fakePlantRepoUtils.setUpMockRepo()
+    UserProfileRepositoryProvider.repository = FakeUserProfileRepository()
+    AchievementsRepositoryProvider.repository = FakeAchievementsRepository()
 
     // Now launch the activity AFTER Firebase is cleaned up
     scenario = ActivityScenario.launch(MainActivity::class.java)
