7ae4829cf1be2cf356e4ac3eeb9a2499496eb4e4
mattmeyer1066 <matteo.meyer@epfl.ch>
2025-12-17 22:01:59 +0100

    fix: Restore suspend function

--- app/src/main/java/com/android/mygarden/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/android/mygarden/ui/feed/FeedViewModel.kt
@@ -177,18 +177,16 @@ class FeedViewModel(
    * @param watchedFriend1 the first friend to watch, or null
    * @param watchedFriend2 the second friend to watch, or null
    */
-  fun setWatchedFriends(watchedFriend1: UserProfile?, watchedFriend2: UserProfile?) {
-    viewModelScope.launch {
-      val relation1 = determineRelationWithUser(watchedFriend1)
-      val relation2 = determineRelationWithUser(watchedFriend2)
+  suspend fun setWatchedFriends(watchedFriend1: UserProfile?, watchedFriend2: UserProfile?) {
+    val relation1 = determineRelationWithUser(watchedFriend1)
+    val relation2 = determineRelationWithUser(watchedFriend2)
 
-      _uiState.value =
-          _uiState.value.copy(
-              watchedUser1 = watchedFriend1,
-              watchedUser2 = watchedFriend2,
-              relationWithWatchedUser1 = relation1,
-              relationWithWatchedUser2 = relation2)
-    }
+    _uiState.value =
+        _uiState.value.copy(
+            watchedUser1 = watchedFriend1,
+            watchedUser2 = watchedFriend2,
+            relationWithWatchedUser1 = relation1,
+            relationWithWatchedUser2 = relation2)
   }
 
   /**
