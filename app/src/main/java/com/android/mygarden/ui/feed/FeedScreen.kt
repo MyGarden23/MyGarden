d19b2df7f438ccb550df82c1c5063519fd1ee6ce
ahuot <adrien.huot@epfl.ch>
2025-12-17 11:01:05 +0100

    feat(ui): adapt achievements card with level
    Adapted the text displayed in the achievement card
    to display the level that has been reached.

--- app/src/main/java/com/android/mygarden/ui/feed/FeedScreen.kt
+++ app/src/main/java/com/android/mygarden/ui/feed/FeedScreen.kt
@@ -373,7 +373,10 @@ fun GotAnAchievementCard(
   val icon = R.drawable.achievement
   val cardText =
       stringResource(
-          R.string.got_achievement_activity, activity.pseudo, activity.achievementType.toString())
+          R.string.got_achievement_activity,
+          activity.pseudo,
+          activity.levelReached,
+          activity.achievementType.toString())
 
   GenericCard(colorPalette, modifier, icon, cardText)
 }
